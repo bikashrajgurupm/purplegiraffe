10823c3ae2d20e418c5b86a9fa2a850a0360d2b1
Bikash Rajguru <bikashrajguru@gmail.com>
2025-08-18 17:27:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -144,14 +144,15 @@ export default function Home() {
             <h1>PurpleGiraffe</h1>
           </div>
           <div className="header-stats">
-            {!emailCaptured && (
+            {/* Question counter disabled for testing */}
+            {/*!emailCaptured && (
               <div className="question-counter">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                   <path d="M21 15a2 2 0 01-2 2H7l-4 4V5a2 2 0 012-2h14a2 2 0 012 2z"/>
                 </svg>
                 <span>{remainingQuestions} of 3 free questions remaining</span>
               </div>
-            )}
+            )}*/}
           </div>
         </div>
       </header>
