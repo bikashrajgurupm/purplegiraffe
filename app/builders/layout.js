b4a303ce86fbf31d3e3ff48fb429fa1ac71c4282
Bikash Rajguru <bikashrajguru@gmail.com>
2026-07-12 12:45:43 +0530

    Create layout.js

---
+++ app/builders/layout.js
@@ -0,0 +1,9 @@
+// app/builders/layout.js
+export const metadata = {
+  title: 'Meet the Builders \u2014 Purple Giraffe',
+  description: 'The people behind the apps at Purple Giraffe.',
+}
+
+export default function BuildersLayout({ children }) {
+  return children
+}
